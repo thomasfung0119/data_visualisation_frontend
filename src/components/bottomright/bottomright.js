d9d008a88eb3648c60e60188f1ca04037b509c99
thomasfung0119 <thomaskkfung@gmail.com>
2022-11-20 20:06:35 +0800

    timeline edit

--- src/components/bottomright/bottomright.js
+++ src/components/bottomright/bottomright.js
@@ -6,6 +6,7 @@ function Bottomright(props) {
   return (
     <div className="bottomright">
       <h1> {props.country} Time Series </h1>
+      <Timeline country = {props.country} />
     </div>
     
   )
--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -4,9 +4,9 @@ import * as d3 from "d3";
 export const Timeline = (country) => {
   const svgRef = useRef(null);
 
-  useEffect(() => {
-    const width = svgRef.current.parentElement.clientWidth;
-    const height = svgRef.current.parentElement.clientHeight;
+  useEffect((country) => {
+    const width = 600;
+    const height = 45;
     // the main svg
     const svg = d3.select(svgRef.current)
       .attr('width', width)
@@ -19,24 +19,79 @@ export const Timeline = (country) => {
     (async () => {
       const margin = { left: 30, top: 10, right: 10, bottom: 20 };
 
-      const xScale = d3.scaleLinear()
+      const xScale = d3.scaleTime()
         .range([margin.left, width - margin.right])
-        .domain([0, 40]);
+        .domain([new Date("2021-08"), new Date("2022-06")]);
       
       svg.append('g')
         .call(d3.axisBottom(xScale))
         .attr('transform', `translate(0,${height - margin.bottom})`);
       
-      // Add circles to svg, more explanation in the cell below
-      svg.selectAll('circle')
-        .data([10, 20, 30, 40])
-        .enter()
-        .append('circle')
-          .attr('cx', d => xScale(d))
-          .attr('cy', 20)
-          .attr('r', 10)
-          .attr('fill', 'SteelBlue')
-
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2021-8")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5)  
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2021-9")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5) 
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2021-10")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5) 
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2021-11")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5) 
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2021-12")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5) 
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-1")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5) 
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-2")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5) 
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-3")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5) 
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-4")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5)
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-5")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5)
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-6")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5)
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-7")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5)
+      svg.append('circle')
+        .style("fill", "Aqua")
+        .attr('cx', xScale(new Date("2022-8")))
+        .attr('cy', 20)
+        .attr('r', Math.floor(Math.random(5)*6)+5)
     })();
   }, []);
 
