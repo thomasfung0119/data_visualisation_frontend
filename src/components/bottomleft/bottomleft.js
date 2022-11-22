8043872047b46969956806fefd2a4bb87ba982f4
chanjeff <jeffchan142@gmail.com>
2022-11-22 14:38:47 +0800

    Revert "Revert "connect buttons""
    This reverts commit 6a263b2c4fd2300a2bb0d155496d715b287fcfb5.

--- src/components/bottomleft/bottomleft.js
+++ src/components/bottomleft/bottomleft.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Globe } from "../globe";
 import "./bottomleft.css";
 import * as d3 from "d3";
@@ -6,11 +6,35 @@ import ButtonGroup from "../buttons/buttongroup";
 
 function Bottomleft(props) {
   const { country, setCountry, data } = props;
+  const [menu, setMenu] = useState("infections");
+
+  const getData = (obj) => {
+    if (!obj) return null;
+    switch (menu) {
+      case "infections":
+        return obj.ConfirmedCase;
+      case "deaths":
+        return obj.MortalityCase;
+      default:
+        return null;
+    }
+  }
+
+  const getInterpolator = () => {
+    switch (menu) {
+      case "infections":
+        return d3.interpolateBlues;
+      case "deaths":
+        return d3.interpolateReds;
+      default:
+        return null;
+    }
+  }
 
   let max = 0;
   for (let key in data) {
-    if (data[key].ConfirmedCase > max) {
-      max = data[key].ConfirmedCase;
+    if (getData(data[key]) > max) {
+      max = getData(data[key]);
     }
   }
 
@@ -18,12 +42,14 @@ function Bottomleft(props) {
     <div className="bottomleft">
       <ButtonGroup
         buttons={[
-          { text: "death", imageURL: "./death.svg" },
-          { text: "infection", imageURL: "./infection.svg" },
+          { text: "deaths", imageURL: "./death.svg" },
+          { text: "infections", imageURL: "./infection.svg" },
         ]}
+        selectedButton={menu}
+        onToggle={(menu) => { setMenu(menu) }}
       />
       {country && (
-        <div class="title">You are checking covid data of {country}</div>
+        <div class="title">You are checking number of {menu} of {country}</div>
       )}
       {!country && (
         <div class="title">Select a country on the Globe to view its data</div>
@@ -33,9 +59,10 @@ function Bottomleft(props) {
           sensitivity={75}
           onClick={(country) => setCountry(country)}
           valueMapper={(country) =>
-            data[country] ? data[country]?.ConfirmedCase / max : null
+            data[country] ? getData(data[country]) / max : null
           }
-          interpolator={d3.interpolateBlues}
+          interpolator={getInterpolator()}
+          maxValue={max}
         />
       )}
     </div>
