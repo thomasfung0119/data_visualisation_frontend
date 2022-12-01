4575c0c58ec12cad24d34658a0545a6036a1262f
wokan0306 <97871987+wokan0306@users.noreply.github.com>
2022-12-01 15:59:22 +0800

    Fixed the timeline bug

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -6,8 +6,9 @@ export const Timeline = (props) => {
   const svgRef = useRef(null);
 
   useEffect(() => {
+    if (!country) return;
     const width = svgRef.current.parentElement.clientWidth;
-    const height = 40;
+    const height = svgRef.current.parentElement.clientHeight*.3;
     // the main svg
     const svg = d3.select(svgRef.current)
       .attr('width', width)
@@ -17,29 +18,34 @@ export const Timeline = (props) => {
     svg.selectAll('*').remove();
 
     //draw timeline
-    (async () => {
-      const margin = { left: 30, top: 10, right: 10, bottom: 20 };
+    fetch('http://127.0.0.1:5000/api/getData?country=' + country)
+    .then(res => res.json())
+    .then(data => {
+      let confirmedCaseData = data.map(({ Date, ConfirmedCase }) => [Date, ConfirmedCase])
+      const margin = { left: width*.07, top: height*.1, right: width*.07, bottom: height*.2 };
 
-      const data = [
-        new Date("2021-08"),
-        new Date("2021-09"),
-        new Date("2021-10"),
-        new Date("2021-11"),
-        new Date("2021-12"),
-        new Date("2022-01"),
-        new Date("2022-02"),
-        new Date("2022-03"),
-        new Date("2022-04"),
-        new Date("2022-05"),
-        new Date("2022-06"),
-      ];
+      const dates = [];
+      let minNo = Infinity;
+      let maxNo = -Infinity;
+      confirmedCaseData.forEach(([dateString, caseNo], i) => {
+        let date = new Date(dateString);
+        confirmedCaseData[i][0] = date;
+        dates.push(date);
+        if (caseNo < minNo) minNo = caseNo;
+        if (caseNo > maxNo) maxNo = caseNo;
+      });
+
+      const linearScale = d3.scaleLinear()
+        .range([0.4, 1])
+        .domain([minNo, maxNo])
 
       const xScale = d3.scaleTime()
         .range([margin.left, width - margin.right])
-        .domain(d3.extent(data));
+        .domain(d3.extent(dates));
 
       // add axis
       const xAxis = d3.axisBottom(xScale)
+        .tickValues(dates)
         .tickFormat(d3.timeFormat('%Y-%m'));
 
       svg.append('g')
@@ -58,14 +64,14 @@ export const Timeline = (props) => {
       svg.append('g')
         .attr("class", "time-line")
         .selectAll("circle")
-        .data(data)
+        .data(confirmedCaseData)
         .enter().append("circle")
         .attr("class", "time-line-bubble")
         .style("fill", "#75e2faa0")
-        .attr('cx', (d) => xScale(d))
-        .attr('cy', 20)
-        .attr('r', (d) => country ? Math.floor(Math.random() * 6) + 5 : 0)
-    })();
+        .attr('cx', (d) => xScale(d[0]))
+        .attr('cy', height*.5)
+        .attr('r', (d) => country ? Math.floor(linearScale(d[1]) * height*.2) : 0)
+    });
   }, [country]);
 
   return (
